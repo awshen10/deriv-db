b8c46ae29968d2f2a39ba7c04e45dc9e973e2301
awshen10 <awshen10@gmail.com>
2026-02-07 19:30:18 +0800

    Clauses

--- Release/v1.0.000/Release_v01/VSIT_RULLALL.sql
+++ Release/v1.0.000/Release_v01/VSIT_RULLALL.sql
@@ -2,5 +2,6 @@
 \echo === START Release v1.0.000 ===
 \ir DB_SCRIPT/02_DATA/01_T_MT_SYS_VER.sql
 \ir DB_SCRIPT/02_DATA/02_T_MT_COUNTRY.sql
+\ir DB_SCRIPT/02_DATA/03_T_MT_CLAUSES.sql
 \echo === END Release v1.0.000 ===
 \o
--- Release/v1.0.000/Release_v01/VSIT_RULLALL_FALLBACK.sql
+++ Release/v1.0.000/Release_v01/VSIT_RULLALL_FALLBACK.sql
@@ -2,5 +2,6 @@
 \echo === START Release v1.0.000 ===
 \ir FALLBACK/02_DATA/01_T_MT_SYS_VER.sql
 \ir FALLBACK/02_DATA/02_T_MT_COUNTRY.sql
+\ir FALLBACK/02_DATA/03_T_MT_CLAUSES.sql
 \echo === END Release v1.0.000 ===
 \o
