fa40f1ec7752f063333efb1df3536ec39e0fc6ce
awshen10 <awshen10@gmail.com>
2026-02-07 19:22:46 +0800

    Updated script

---
+++ Release/v1.0.000/Release_v01/FALLBACK/02_DATA/02_T_MT_COUNTRY.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS t_mt_country;
--- Release/v1.0.000/Release_v01/VSIT_RULLALL.sql
+++ Release/v1.0.000/Release_v01/VSIT_RULLALL.sql
@@ -1,5 +1,6 @@
 \o Release_v01.log
 \echo === START Release v1.0.000 ===
-\ir DB_SCRIPT/02_DATA/T_MT_SYS_VER.sql
+\ir DB_SCRIPT/02_DATA/01_T_MT_SYS_VER.sql
+\ir DB_SCRIPT/02_DATA/02_T_MT_COUNTRY.sql
 \echo === END Release v1.0.000 ===
 \o
